5c1dbffd6fab16c7f672791b4052183e5ec36ad5
josh sofela <joshuasofela@gmail.com>
2023-10-30 07:23:34 -0700

    collection section

--- src/app/page.js
+++ src/app/page.js
@@ -12,6 +12,7 @@ import BestSellers from "@/components/BestSellers";
 import cat1 from "@/Assets/cat-large-item1.jpg"
 import cat2 from "@/Assets/cat-large-item2.jpg"
 import cat3 from "@/Assets/cat-large-item3.jpg"
+import col from "@/Assets/collection-banner.jpg"
 
 
 
@@ -23,99 +24,121 @@ export default function Home() {
       <Homep />
 
     <div className="flex justify-center ">
-        <div className='h-[40vh] max-lg:h-[30vh] max-md:h-[40vh] max-sm:h-[50vh] flex text-center justify-evenly max-sm:grid max-sm:grid-cols-2 max-sm:py-10'>
-          <div className="w-1/5 flex flex-col justify-center items-center max-sm:w-full max-sm:py-">
-            <p><AiOutlineBook className='text-[30px] text-center text-red-300'/></p>
-            <h1 className='text-[2vw] max-sm:text-[5vw] '>Book Appointment</h1>
-            <p className="text-slate-500 max-md:text-[13px] max-sm:pb-7 max-sm:w-11/12">At imperdiet dui accumsan sit amet nulla risus est ultricies quis.</p>
+          <div className='h-[40vh] max-lg:h-[30vh] max-md:h-[40vh] max-sm:h-[50vh] flex text-center justify-evenly max-sm:grid max-sm:grid-cols-2 max-sm:py-10'>
+            <div className="w-1/5 flex flex-col justify-center items-center max-sm:w-full max-sm:py-">
+              <p><AiOutlineBook className='text-[30px] text-center text-red-300'/></p>
+              <h1 className='text-[2vw] max-sm:text-[5vw] '>Book Appointment</h1>
+              <p className="text-slate-500 max-md:text-[13px] max-sm:pb-7 max-sm:w-11/12">At imperdiet dui accumsan sit amet nulla risus est ultricies quis.</p>
+            </div>
+            <div className="w-1/5 flex flex-col justify-center items-center max-sm:w-full  max-sm:py-">
+              <p><BsHandbag className='text-[30px] text-center text-red-300'/></p>
+              <h1 className='text-[2vw] max-sm:text-[5vw]'>Pick Up in A Store</h1>
+              <p className="text-slate-500 max-md:text-[13px] max-sm:pb-7  max-sm:w-11/12">At imperdiet dui accumsan sit amet nulla risus est ultricies quis.</p>
+              </div>
+            <div className="w-1/5 flex flex-col justify-center items-center max-sm:w-full  max-sm:py-">
+              <p><AiOutlineGift className='text-[30px] text-center text-red-300'/></p>
+              <h1 className='text-[2vw] max-sm:text-[5vw]'>Special Packaging</h1>
+              <p className="text-slate-500 max-md:text-[13px] max-sm:w-11/12">At imperdiet dui accumsan sit amet nulla risus est ultricies quis.</p>
+              </div>
+            <div className="w-1/5 flex flex-col justify-center items-center max-sm:w-full max-sm:py-">
+              <p><HiOutlineRefresh className='text-[30px] text-center text-red-300'/></p>
+              <h1 className='text-[2vw] max-sm:text-[5vw]'>Free global return</h1>
+              <p className="text-slate-500 max-md:text-[13px] max-sm:w-11/12 ">At imperdiet dui accumsan sit amet nulla risus est ultricies quis.</p>
+              </div>
           </div>
-          <div className="w-1/5 flex flex-col justify-center items-center max-sm:w-full  max-sm:py-">
-            <p><BsHandbag className='text-[30px] text-center text-red-300'/></p>
-            <h1 className='text-[2vw] max-sm:text-[5vw]'>Pick Up in A Store</h1>
-            <p className="text-slate-500 max-md:text-[13px] max-sm:pb-7  max-sm:w-11/12">At imperdiet dui accumsan sit amet nulla risus est ultricies quis.</p>
-            </div>
-          <div className="w-1/5 flex flex-col justify-center items-center max-sm:w-full  max-sm:py-">
-            <p><AiOutlineGift className='text-[30px] text-center text-red-300'/></p>
-            <h1 className='text-[2vw] max-sm:text-[5vw]'>Special Packaging</h1>
-            <p className="text-slate-500 max-md:text-[13px] max-sm:w-11/12">At imperdiet dui accumsan sit amet nulla risus est ultricies quis.</p>
-            </div>
-          <div className="w-1/5 flex flex-col justify-center items-center max-sm:w-full max-sm:py-">
-            <p><HiOutlineRefresh className='text-[30px] text-center text-red-300'/></p>
-            <h1 className='text-[2vw] max-sm:text-[5vw]'>Free global return</h1>
-            <p className="text-slate-500 max-md:text-[13px] max-sm:w-11/12 ">At imperdiet dui accumsan sit amet nulla risus est ultricies quis.</p>
-            </div>
-        </div>
     </div>
 
     <div className="">
-        <div className="flex justify-center flex-col items-center">
-          <h1 className="text-[30px] max-sm:text-[20px]" style={{fontFamily: "itl"}}>NEW ARRIVALS</h1>
-          <div className="w-11/12 pt-4">
+          <div className="flex justify-center flex-col items-center">
+            <h1 className="text-[30px] max-sm:text-[20px]" style={{fontFamily: "itl"}}>NEW ARRIVALS</h1>
+            <div className="w-11/12 pt-4">
 
-            <ProdCaro >
-               {
-                prod.map(product =>(
-                  <NewArrivals product={product} />
-              ))}
-            </ProdCaro>
-           
+              <ProdCaro >
+                {
+                  prod.map(product =>(
+                    <NewArrivals product={product} />
+                ))}
+              </ProdCaro>
+            
+            </div>
+            <h1 className="py-5 font-medium">VIEW ALL PRODUCTS</h1>
+
+            <h1 className="text-[30px] max-sm:text-[20px] pt-14" style={{fontFamily: "itl"}}>BEST SELLERS</h1>
+            <div className="w-11/12 pt-4">
+
+              <ProdCaro >
+                {
+                  prod.map(product =>(
+                    <BestSellers product={product} />
+                ))}
+              </ProdCaro>
+            
+            </div>
+            <h1 className="py-5 font-medium">VIEW ALL PRODUCTS</h1>
           </div>
-          <h1 className="py-5 font-medium">VIEW ALL PRODUCTS</h1>
-
-          <h1 className="text-[30px] max-sm:text-[20px] pt-14" style={{fontFamily: "itl"}}>BEST SELLERS</h1>
-          <div className="w-11/12 pt-4">
-
-            <ProdCaro >
-               {
-                prod.map(product =>(
-                  <BestSellers product={product} />
-              ))}
-            </ProdCaro>
-           
-          </div>
-          <h1 className="py-5 font-medium">VIEW ALL PRODUCTS</h1>
-        </div>
     </div>
 
     <div className="grid grid-cols-2 max-sm:grid-cols-1 text-white">
 
-        <div className="relative w-full">
-          <Image src={cat1} className="w-full max-sm:w-half"/>
-          <div className="absolute top-1/3 w-3/4 max-lg:w-3/4">
-              <div className="  z-10 border-4  max-lg:border-2 border-l-0 border-white px-6 py-8">
-               <h1 className="text-[3.5vw] w-1/2 max-lg:text-[2.5vw] max-sm:text-[5vw]"> SKIN CARE</h1>
-               <button className="border-white-500 border bg-none text-[1.2vw] p-2 max-md:text-[1.8vw]">SHOP NOW</button>
+          <div className="relative w-full">
+            <Image src={cat1} className="w-full max-sm:w-half"/>
+            <div className="absolute top-1/3 w-3/4 max-lg:w-3/4">
+                <div className="  z-10 border-4  max-lg:border-2 border-l-0 border-white px-6 py-8">
+                <h1 className="text-[3.5vw] w-1/2 max-lg:text-[2.5vw] max-sm:text-[5vw]"> SKIN CARE</h1>
+                <button className=" bg-red-200 rounded-md bg-opacity-75 text-[1.2vw] p-2 max-md:text-[1.8vw] max-sm:mt-3">SHOP NOW</button>
+                </div>
               </div>
-            </div>
-        </div>
-
-        
-        <div className="">
-          <div className="relative">
-            <Image src={cat2} className="w-full"/>
-            <div className="absolute top-0 w-3/4">
-              <div className="  z-10 border-4 max-lg:border-2 border-t-0 border-white  px-6 py-8 max-md:py-3 max-sm:py-5">
-               <h1 className="text-[3.5vw] w-1/2 max-lg:text-[2.5vw] max-sm:text-[5vw]">MAKE-UP PRODUCTS</h1>
-               <button className="border-white-500 border bg-none text-[1.2vw] p-2 max-md:text-[1.8vw]">SHOP NOW</button>
-              </div>
-            </div>
           </div>
 
-          <div className="relative">
-            <Image src={cat3} className="w-full absolute"/>
-            <div className="absolute w-3/4">
-              <div className="  z-10 border-4  max-lg:border-2 border-t-0 border-white  px-6 py-8 max-md:py-3 max-sm:py-5">
-               <h1 className="text-[3.5vw] w-1/2 max-lg:text-[2.5vw] max-sm:text-[5vw]">BEAUTY PRODUCTS</h1>
-               <button className="border-white-500 border bg-none text-[1.2vw] p-2 max-md:text-[1.8vw]">SHOP NOW</button>
+          
+          <div className="">
+            <div className="relative">
+              <Image src={cat2} className="w-full"/>
+              <div className="absolute top-0 w-3/4">
+                <div className="  z-10 border-4 max-lg:border-2 border-t-0 border-white  px-6 py-8 max-md:py-3 max-sm:py-5">
+                <h1 className="text-[3.5vw] w-1/2 max-lg:text-[2.5vw] max-sm:text-[5vw]">MAKE-UP PRODUCTS</h1>
+                <button className=" bg-red-200 rounded-md bg-opacity-75 bg-none text-[1.2vw] p-2 max-md:text-[1.8vw]">SHOP NOW</button>
+                </div>
               </div>
             </div>
+
+            <div className="relative">
+              <Image src={cat3} className="w-full absolute"/>
+              <div className="absolute w-3/4">
+                <div className="  z-10 border-4  max-lg:border-2 border-t-0 border-white  px-6 py-8 max-md:py-3 max-sm:py-5">
+                <h1 className="text-[3.5vw] w-1/2 max-lg:text-[2.5vw] max-sm:text-[5vw]">BEAUTY PRODUCTS</h1>
+                <button className=" bg-red-200 rounded-md bg-opacity-75 bg-none text-[1.2vw] p-2 max-md:text-[1.8vw]">SHOP NOW</button>
+                </div>
+              </div>
+            </div>
+
           </div>
 
+          
+    </div>
+      
+    <div className="w-full py-8 ">
+      <div className="flex w-full relative justify-center items-center max-md:hidden">
+        <div className="w-10/12 flex justify-center items-center">
+          <h1 className="absolute text-white text-[5vw] w-40 h-40 self-start justify-self-start" style={{fontFamily:'itl'}}><span className="line-through">25%</span> NOW 50% OFF</h1>
+          <Image src={col} className="w-[60%] "/>
         </div>
 
-        
+        <div className="w-11/12">
+          <h1 className="text-[5vw] max-md:text-[4vw]">SUMMER COLLECTION</h1>
+          <p className="w-[90%] text-slate-400 text-[13px] max-md:text-[10px]">Dignissim lacus, turpis ut suspendisse vel tellus.
+             Turpis purus, gravida orci, fringilla a. Ac sed eu
+              fringilla odio mi. Consequat pharetra at magna 
+              imperdiet cursus ac faucibus sit libero. Ultricies 
+              quam nunc, lorem sit lorem urna, pretium aliquam ut.
+               In vel, quis donec dolor id in. Pulvinar commodo mollis 
+               diam sed facilisis at cursus imperdiet cursus ac faucibus 
+               sit faucibus sit libero.
+          </p>
+          <button className="border-2 border-black font-medium mt-4 p-2">SHOP COLLECTION</button>
+        </div>
       </div>
-
+    </div>
     </div>
         
    
