6bde7b8ab8ecf91d2ca9d29a678fa8311cbb644e
josh sofela <joshuasofela@gmail.com>
2023-11-08 11:38:53 -0800

    update

--- src/app/page.js
+++ src/app/page.js
@@ -17,10 +17,37 @@ import arr from "@/Assets/c3.svg"
 
 import Suscribe from "@/components/Suscribe";
 
+import { useEffect, useState } from "react";
 
 
 export default function Home() {
-  console.log(prod);
+  const [all, setAll] =useState([])
+
+  useEffect(()=>{
+    const fetchData = async () => {
+      const url = 'https://asos2.p.rapidapi.com/products/v2/list?store=US&offset=0&categoryId=4209&limit=48&country=US&sort=freshness&q=tshirt&currency=USD&sizeSchema=US&lang=en-US';
+      const options = {
+        method: 'GET',
+        headers: {
+          'X-RapidAPI-Key': 'f71d2a5c79msh54724eb70b12fa9p1c0d01jsn6e622751224a',
+          'X-RapidAPI-Host': 'asos2.p.rapidapi.com'
+        }
+      };
+      
+      try {
+        const response = await fetch(url, options);
+        const result = await response.json();
+        setAll(result.products)
+        console.log(result.products[0].imageUrl);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+
+    fetchData(); // Call the asynchronous function to fetch the data
+  }, []); 
+
+
   return (
     <div
       className=" text-black "
@@ -47,7 +74,7 @@ export default function Home() {
 
           <div className="w-11/12 pt-4">
             <ProdCaro>
-              {prod.map((product) => (
+              {all.map((product) => (
                 <NewArrivals product={product} />
               ))}
             </ProdCaro>
--- src/components/NewArrivals.jsx
+++ src/components/NewArrivals.jsx
@@ -1,19 +1,21 @@
 import React from "react";
 import Image from "next/image";
+import p4 from "@/Assets/img-all1.png"
 
 export default function NewArrivals({product}){
+  const { imageUrl, additionalImageUrls, name, price } = product;
     return (
       <div className="flex justify-evenly" key={product.id}>
         <div className="w-10/12">
-          <Image src={product.img} className="rounded-md" />
+          <Image src={p4} className="w-11/12" />
           <div>
             <h1
               className="text-[20px] max-sm:text-[10px]"
               style={{ fontFamily: "ptserifr" }}
             >
-              {product.title}
+              {name}
             </h1>
-            <p className="max-sm:text-[10px]">{product.price}</p>
+            <p className="max-sm:text-[10px]"></p>
           </div>
         </div>
       </div>
