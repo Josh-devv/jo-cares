08e6c1a3d1e7b01c2b08e8e4bd3ef5b77dd41ee9
josh sofela <joshuasofela@gmail.com>
2024-01-06 10:53:23 -0800

    update

--- src/app/prods/[productId]/page.jsx
+++ src/app/prods/[productId]/page.jsx
@@ -77,7 +77,7 @@ export default function Prods({params}) {
             {
               showPopup ? (
                 <div className='fixed top-0 pt-10 w-[100%] left-1/2 text-white h-[20vh] flex justify-center items-end font-rbt transform -translate-x-1/2 -translate-y-1/2 bg-green-400 p-8 rounded shadow-md z-50'>
-                <p>Item has been added to the Cart!!!</p>
+                <p>Item has already been to the Cart!!!</p>
               </div>
            
               ) : (
