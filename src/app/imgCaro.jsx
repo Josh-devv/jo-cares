5497bf4b22a84d166bd5d4ac5bd139afb3c1219e
josh sofela <joshuasofela@gmail.com>
2024-01-12 10:16:14 -0800

    Finalising

--- src/app/imgCaro.jsx
+++ src/app/imgCaro.jsx
@@ -6,7 +6,7 @@ import p2 from "@/Assets/p-038.jpg"
 import p3 from "@/Assets/p-020.jpg"
 import p4 from "@/Assets/p-019.jpg"
 import p5 from "@/Assets/p-018.jpg"
-import t1 from "@/Assets/tees1.webp"
+import new10 from "@/Assets/stripe.jpg"
 import prr from "@/Assets/prads.png"
 import prr1 from "@/Assets/burr.png"
 import prr2 from "@/Assets/loi2.png"
@@ -18,7 +18,15 @@ import m3 from "@/Assets/man2.jpg"
 import m4 from "@/Assets/man3.jpg"
 import m5 from "@/Assets/man4.jpg"
 import m6 from "@/Assets/man5.jpg"
-
+import new1 from "@/Assets/bike.jpg"
+import new2 from "@/Assets/jeans.jpg"
+import new3 from "@/Assets/long1.jpg"
+import new4 from "@/Assets/long.jpg"
+import new5 from "@/Assets/coat.jpg"
+import new6 from "@/Assets/coat2.jpg"
+import new7 from "@/Assets/back.jpg"
+import new8 from "@/Assets/show3.jpg"
+import new9 from "@/Assets/jean.jpg"
 
 import { AiOutlineBook } from "react-icons/ai";
 import { BsHandbag} from "react-icons/bs";
@@ -101,42 +109,42 @@ export const prod2 =  [
         id:1,
         img: p2,
         title: "SOLID OPEN FRONT BLAZER",
-        price: 159.99
+        price: 159.00
     },
     {
         id:2,
         img: p1,
         title: "PANTS SUIT CAMI TOP",
-        price: 99.99
+        price: 99.00
     },
     {
         id:3,
         img: p3,
         title: "LAPEL BREASTED BLAZER",
-        price: 79.99
+        price: 79.00
     },
     {
         id:4,
         img: p4,
         title: "SOLID NECK BLAZER",
-        price: 99.99
+        price: 99.00
     },
     {
         id:5,
         img: p5,
         title: "SOLID FORM FITTED TEE",
-        price: 49.99
+        price: 49.00
     },
     {
         id:6,
-        img: p6,
-        title: "1PC SOLID FORM FITTED TEE",
-        price: 29.99
+        img: new10,
+        title: "SOLID STRIPES TEE",
+        price: 29.00
     },
     {
         id:7,
-        img: p7,
-        title: "NATURAL GLOW",
+        img: new8,
+        title: "DENIM CROP TOP",
         price: 95.00
     },
     {
@@ -152,7 +160,7 @@ export const prod =  [
         id: 9,
         img: m2,
         title: "NOTCHED DENIM",
-        price: 99.99
+        price: 99.00
     },
     {
         id: 10,
@@ -164,31 +172,31 @@ export const prod =  [
         id:11,
         img: m4,
         title: "SOLID OPEN COAT",
-        price: 99.99
+        price: 99.00
+    },
+    {
+        id: 24,
+        img: new1,
+        title: "PRIVE FRONT COAT",
+        price: 99.00
+    },
+    {
+        id: 23,
+        img: new2,
+        title: "DOUBLE LEG JEAN",
+        price: 99.00
+    },
+    {
+        id: 22,
+        img: new3,
+        title: "PRIVE BROWN COAT",
+        price: 99.00
     },
     {
         id: 12,
-        img: m5,
-        title: "PRIVE FRONT COAT",
-        price: 99.99
-    },
-    {
-        id: 12,
-        img: m5,
-        title: "PRIVE FRONT COAT",
-        price: 99.99
-    },
-    {
-        id: 12,
-        img: m5,
-        title: "PRIVE FRONT COAT",
-        price: 99.99
-    },
-    {
-        id: 12,
-        img: m5,
-        title: "PRIVE FRONT COAT",
-        price: 99.99
+        img: new4,
+        title: "WIDE BLOUSE",
+        price: 99.00
     },
    
     {
@@ -222,13 +230,13 @@ export const prod3 =  [
     {
         id: 14,
         img: p1,
-        title: "CLEAN SLATE CREAM TEE",
+        title: "CLEAN SLATE BLUE TEE",
         price: 48.00
     },
     {
         id:15,
         img: p3,
-        title: "SIMPLE WHITE COAT ",
+        title: "SIMPLE CREAM COAT ",
         price: 127.00
     },
     {
@@ -238,19 +246,19 @@ export const prod3 =  [
         price: 120.00
     },
     {
-        id:17,
-        img: p5,
-        title: "PURE PINK COTTON SHIRT",
+        id:21,
+        img: new6,
+        title: "PURE BROWN COTTON SHIRT",
         price: 89.00
     },
     {
-        id:17,
-        img: p5,
-        title: "PURE PINK COTTON SHIRT",
+        id:20,
+        img: new5,
+        title: "THICK DOUBLE COAT",
         price: 89.00
     },
     {
-        id:17,
+        id:19,
         img: p5,
         title: "PURE PINK COTTON SHIRT",
         price: 89.00
@@ -265,7 +273,7 @@ export const prod3 =  [
     {
         id: 18,
         img: m1,
-        title: "CLEAN SLATE COAT COVER",
+        title: "CLEAN BROWN COAT COVER",
         price: 99.00
     },/*
     {
@@ -294,44 +302,55 @@ export const allProd=[
         id:1,
         img: p2,
         title: "SOLID OPEN FRONT BLAZER",
-        price: 159.99
+        price: 159.00
     },
     {
         id:2,
         img: p1,
         title: "PANTS SUIT CAMI TOP",
-        price: 99.99
+        price: 99.00
     },
     {
         id:3,
         img: p3,
         title: "LAPEL BREASTED BLAZER",
-        price: 79.99
+        price: 79.00
     },
     {
         id:4,
         img: p4,
         title: "SOLID NECK BLAZER",
-        price: 99.99
+        price: 99.00
     },
     {
         id:5,
         img: p5,
         title: "SOLID FORM FITTED TEE",
-        price: 49.99
+        price: 49.00
     },
-   
+    {
+        id:6,
+        img: new10,
+        title: "SOLID STRIPES TEE",
+        price: 29.00
+    },
+    {
+        id:7,
+        img: new8,
+        title: "DENIM CROP TOP",
+        price: 95.00
+    },
     {
         id:8,
         img: m1,
         title: "NATURAL GLOW",
         price: 95.00
-    },
-    {
+    }
+    ,{
         id: 9,
         img: m2,
         title: "NOTCHED DENIM",
-        price: 99.99
+        price: 99.00
     },
     {
         id: 10,
@@ -343,13 +362,31 @@ export const allProd=[
         id:11,
         img: m4,
         title: "SOLID OPEN COAT",
-        price: 99.99
+        price: 99.00
+    },
+    {
+        id: 24,
+        img: new1,
+        title: "PRIVE FRONT COAT",
+        price: 99.00
+    },
+    {
+        id: 23,
+        img: new2,
+        title: "DOUBLE LEG JEAN",
+        price: 99.00
+    },
+    {
+        id: 22,
+        img: new3,
+        title: "PRIVE BROWN COAT",
+        price: 99.00
     },
     {
         id: 12,
-        img: m5,
-        title: "PRIVE FRONT COAT",
-        price: 99.99
+        img: new4,
+        title: "WIDE BLOUSE",
+        price: 99.00
     },
    
     {
@@ -361,13 +398,13 @@ export const allProd=[
     {
         id: 14,
         img: p1,
-        title: "CLEAN SLATE CREAM TEE",
+        title: "CLEAN SLATE BLUE TEE",
         price: 48.00
     },
     {
         id:15,
         img: p3,
-        title: "SIMPLE WHITE COAT ",
+        title: "SIMPLE CREAM COAT ",
         price: 127.00
     },
     {
@@ -375,6 +412,24 @@ export const allProd=[
         img: p4,
         title: "ESSENTIAL GRAY CROP SHIRT",
         price: 120.00
+    },
+    {
+        id:21,
+        img: new6,
+        title: "PURE BROWN COTTON SHIRT",
+        price: 89.00
+    },
+    {
+        id:20,
+        img: new5,
+        title: "THICK DOUBLE COAT",
+        price: 89.00
+    },
+    {
+        id:19,
+        img: p5,
+        title: "PURE PINK COTTON SHIRT",
+        price: 89.00
     },
     {
         id:17,
@@ -386,7 +441,7 @@ export const allProd=[
     {
         id: 18,
         img: m1,
-        title: "CLEAN SLATE COAT COVER",
+        title: "CLEAN BROWN COAT COVER",
         price: 99.00
     },
 ]
