32529df685babd03e645d317ec0f093fb1b48dbc
josh sofela <joshuasofela@gmail.com>
2024-01-13 08:47:23 -0800

    new commit

--- src/app/Navbar.jsx
+++ src/app/Navbar.jsx
@@ -64,20 +64,7 @@ function Navbar() {
   const [prevScrollPos, setPrevScrollPos] = useState(0);
   const [visible, setVisible] = useState(true);
 
-  /*useEffect(() => {
-    const handleScroll = () => {
-      const currentScrollPos = window.pageYOffset;
 
-      setVisible(prevScrollPos > currentScrollPos || currentScrollPos < 10);
-      setPrevScrollPos(currentScrollPos);
-    };
-
-    window.addEventListener('scroll', handleScroll);
-    return () => {
-      window.removeEventListener('scroll', handleScroll);
-    };
-  }, [prevScrollPos]);
-*/
   return (
     <>
     
